d742a8dc3a3482e6180974ba050f48cfa384043f
Ro Sharp <ro.moreko@gmail.com>
2022-09-12 13:55:59 +0500

    Product Image Carousel

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,50 +1,55 @@
 import React, { Component } from 'react'
 import parse from "html-react-parser";
+import { isTypeSystemDefinitionNode } from 'graphql';
 
 export default class Product extends Component {
     constructor(props) {
         super(props);
         this.toggleActive = this.toggleActive.bind(this);
-        this.state = { imageActive: false }
+        this.state = { 
+            activeImg: 0,
+        }
     }
 
-    toggleActive() {
-        this.setState(prevState => ({
-            imageActive: !prevState.imageActive
-        }));
+    toggleActive(e) {
+        this.setState({ activeImg: parseInt(e.target.id) });
     }
 
     render() {
 
         return (
             <div>
-                <div>
-                    {this.props.gallery.filter((e, i) => i === 0).map((url, index) => {
-                        return (
-                            <img
-                                id={`img-${index}`}
-                                src={url}
-                                alt="product"
-                                key={index}
-                                className={`product-img ${this.state.imageActive ? "img-active" : ""}`}
-                                onClick={this.toggleActive}
-                            />
-                        );
-                    })}
-
-                    {this.props.gallery.filter((e, i) => i !== 0).map((url, index) => {
-                        return (
-                            <img
-                                id={`img-${index + 1}`}
-                                src={url}
-                                alt="product"
-                                key={index + 1}
-                                className={`product-img ${this.state.imageActive ? "img-active" : ""}`}
-                                onClick={this.toggleActive}
-                            />
-                        );
-                    })}
+                <div className="product-img-container">
+                    <div className="inactive-img-container">
+                        {this.props.gallery.map((item, index) => {
+                            return (
+                                <img
+                                    id={index}
+                                    src={item}
+                                    alt="product"
+                                    key={index}
+                                    className="product-img img-inactive"
+                                    onClick={this.toggleActive}
+                                />
+                            );
+                        })}
+                    </div>
+                    <div className="active-img-container">
+                        {this.props.gallery.filter((e, i) => i === this.state.activeImg).map((item, index) => {
+                            return (
+                                <img
+                                    id={this.state.activeImg}
+                                    src={item}
+                                    alt="product"
+                                    key={index}
+                                    className="product-img img-active"
+                                    onClick={this.toggleActive}
+                                />
+                            );
+                        })}
+                    </div>
                 </div>
+                 
                 <h1>{this.props.name} - {this.props.brand}</h1>
                 {parse(this.props.description)}
             </div>
